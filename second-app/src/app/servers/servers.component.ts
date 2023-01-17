3699bc57e02d7dbc275d121d543a0b2506f96a03
tanbbang01 <tanbbang01@gmail.com>
2023-01-18 07:56:21 +0900

    Other using by selector From Component

--- second-app/src/app/servers/servers.component.ts
+++ second-app/src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 // $ ng generate component servers
 @Component({
+  // Commmon Usaage
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
