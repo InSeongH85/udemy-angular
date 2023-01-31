9b865d44dc701be9b335495353865ef3969d09c0
tanbbang01 <tanbbang01@gmail.com>
2023-02-01 08:01:50 +0900

    Study for View capsulation

--- forth-app/src/app/server-element/server-element.component.ts
+++ forth-app/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
+  encapsulation: ViewEncapsulation.Emulated //Emulated 기본값이다. None, ShadowDom
 })
 export class ServerElementComponent {
   @Input("srvElement") element: { type: string; name: string; content: string };
