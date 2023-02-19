3028ab66ce0fe380ff9d3731e54b2e8479a515a4
tanbbang01 <tanbbang01@gmail.com>
2023-02-20 08:12:29 +0900

    Use HostBinding instead of Renderer2

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -4,18 +4,21 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@an
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = 'blue'
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    this.backgroundColor  = 'transparent'
   }
 
 }
