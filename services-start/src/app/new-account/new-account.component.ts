89ff9efb6a15ea31042d45242b8dd9f2b97fa7e5
InSeongHwang <tanbbang01@InSeongHwangui-MacBookPro.local>
2023-02-22 22:19:23 +0900

    Created Service & Use Dependency injection

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from "@angular/core";
+import { AccountsService } from "../accounts.service";
+import { LoggingService } from "../logging.services";
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
+  // providers 로 Service 를 Dependency injection
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // Service Injection
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
